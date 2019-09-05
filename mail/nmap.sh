2a92b54d48b3a00f6de1d1c94b90fc91c48c4d40
xiuling <xiulingorigin@163.com>
2019-09-05 17:10:34 +0000

    add nmap.sh

---
+++ mail/nmap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+nmap -PS 192.168.31.0/24 > /root/nmap
+unix2dos /root/nmap
+zip nmap.zip /root/nmap
+echo "Hosr Discovery" | mutt -s "Nmap Scan Report" xiulingorigin@163.com -a /root/nmap.zip
+rm -rf /root/nmap /root/nmap.zip
