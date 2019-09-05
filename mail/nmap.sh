fb6d497025ebd842071d4951a3e17a77349b83ba
xiuling <xiulingorigin@163.com>
2019-09-05 17:45:47 +0000

    fix nmap.sh

--- mail/nmap.sh
+++ mail/nmap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+touch /root/nmap
 nmap -PS 192.168.31.0/24 > /root/nmap
 unix2dos /root/nmap
-zip nmap.zip /root/nmap
+zip /root/nmap.zip /root/nmap
 echo "Hosr Discovery" | mutt -s "Nmap Scan Report" xiulingorigin@163.com -a /root/nmap.zip
 rm -rf /root/nmap /root/nmap.zip
